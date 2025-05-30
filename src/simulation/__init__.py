e0c094a7eeb035123e5c01357ed04e4dca97b4bf
Montreal.AI <info@montreal.ai>
2025-05-29 23:09:43 -0400

    [alpha_factory] Add MATS operators and tests

---
+++ src/simulation/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+"""Lightweight simulation helpers."""
+
+from .mats_ops import GaussianParam, PromptRewrite, CodePatch
+
+__all__ = ["GaussianParam", "PromptRewrite", "CodePatch"]
